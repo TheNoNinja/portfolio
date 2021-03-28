e7a9058f1bb35a5831eb6cf735f761174cc30fce
TheNoNinja <christiaanbrant@gmail.com>
2021-03-28 18:11:03 +0000

    Added CRUD to the API for articles

--- Api/routes/articles.js
+++ Api/routes/articles.js
@@ -3,12 +3,69 @@ const router = express.Router();
 
 const Article = require('../models/Article');
 
-router.get('/', (req, res) =>{
-    req.send("all articles");
+router.get('/', async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.status(200).json(articles);
+    } 
+    catch (error) {
+        res.status(500).json({message: "Error while getting articles.", errors: error.errors});
+    }
 });
 
-router.post('/', (req, res) =>{
-    console.log(req.body);
+router.get('/:articleId', async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.articleId);
+        res.status(200).json(article);
+    } 
+    catch (error) {
+        res.status(500).json({message: "Error while getting article.", errors: error.errors});
+    }
+});
+
+router.post('/', async (req, res) => {
+    const article = new Article({
+        title: req.body.title,
+        blurb: req.body.blurb,
+        content: req.body.content,
+        thumbnail: req.body.thumbnail
+    });
+    
+    try {
+        const savedArticle = await article.save();
+        res.status(200).json(savedArticle);
+    }
+    catch (error){
+        res.status(500).json({message: "Error while saving article.", errors: error.errors});
+    }
+});
+
+router.delete('/:articleId', async (req, res) => {
+    try {
+        const deletedArticle = Article.remoce({_id: req.params.articleId});
+        res.status(200).json(deletedArticle);
+    } 
+    catch (error) {
+        res.status(500).json({message: "Error while deleting article.", errors: error.errors});
+    }
+});
+
+router.patch('/:articleId', async (req, res) => {
+    try {
+        const article = await Article.findById(req.params.articleId);
+        const updatedArticle = await Article.updateOne({_id: req.params.articleId}, {
+            $set: {
+                title: req.body.title != undefined ? req.body.title : article.title,
+                date: req.body.date != undefined ? req.body.date : article.date,
+                blurb: req.body.blurb != undefined ? req.body.blurb : article.blurb,
+                content: req.body.content != undefined ? req.body.content : article.content,
+                thumbnail: req.body.thumbnail != undefined ? req.body.thumbnail : article.thumbnail
+            }
+        });
+        res.status(200).json(updatedArticle);
+    } catch (error) {
+        res.status(500).json({message: "Error while updating article.", errors: error.errors});
+    }
 });
 
 module.exports = router;
--- WebApp/src/components/Content.vue
+++ WebApp/src/components/Content.vue
@@ -1,6 +1,6 @@
 <template>
     <main>
-        <h1>MainContent</h1>
+        <h1>Test</h1>
     </main>
 </template>
 
